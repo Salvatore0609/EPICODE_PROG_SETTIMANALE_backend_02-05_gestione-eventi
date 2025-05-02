80447cc51563651c3c2f8d893a8d03e9fa7b123f
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-05-02 20:09:51 +0200

    Implement user authentication and authorization with JWT support

--- src/main/java/it/epicode/gestione_eventi/auth/GestioneEventiApplication.java
+++ src/main/java/it/epicode/gestione_eventi/auth/GestioneEventiApplication.java
@@ -1,4 +1,4 @@
-package it.epicode.gestione_eventi;
+package it.epicode.gestione_eventi.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
